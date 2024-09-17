13ff06aa37de1b97afc58a32b77b03931bac15a9
ChandanSayed <hangzhou635@gmail.com>
2024-09-17 13:04:41 +0600

    emailLogin

--- app/todos/page.tsx
+++ app/todos/page.tsx
@@ -1,8 +1,18 @@
 import { TodoList } from "@/components/todo-list";
 import { Separator } from "@/components/ui/separator";
+import { createClient } from "@/utils/supabase/server";
+import { redirect } from "next/navigation";
 
 export default async function TodosPage() {
   const todos = ["This is a todo"];
+  const supabase = createClient();
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
+  if (!user) {
+    return redirect("/");
+  }
 
   return (
     <section className="p-3 pt-6 max-w-2xl w-full flex flex-col gap-4">
